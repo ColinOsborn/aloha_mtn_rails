6c387fe69dd717e70a7c84a2beb65f1eac0bf3b1
Colin Osborn <colin.osborn@gmail.com>
2020-06-10 08:52:37 -0600

    implemented paperclip to allow for image uploads

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_09_204338) do
+ActiveRecord::Schema.define(version: 2020_06_10_035725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bikes", force: :cascade do |t|
     t.string "name"
-    t.string "image_path"
     t.boolean "rental"
     t.boolean "demo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
