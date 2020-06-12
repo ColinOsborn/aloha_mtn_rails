49f22ea37d2b81eb3cbb7646d86a95a434914051
Colin Osborn <colin.osborn@gmail.com>
2020-06-12 10:12:01 -0600

    implemeted index page of blog posts

--- spec/features/blog/user_views_a_list_of_blog_posts_spec.rb
+++ spec/features/blog/user_views_a_list_of_blog_posts_spec.rb
@@ -16,4 +16,25 @@ RSpec.feature "Listing blogs" do
         expect(page).to have_link(blog1.title)
         expect(page).to have_link(blog2.title)
     end
+
+     scenario "A user has no articles created" do
+        blog1 = Blog.create(title: "First Article", body: "Lorem ipsum dolor sit amet, consectetur")
+        blog2 = Blog.create(title: "Second Article", body: "Body of the 2nd article")
+
+        Blog.delete_all
+
+         visit "/blogs"
+
+        expect(page).not_to have_content(blog1.title)
+        expect(page).not_to have_content(blog1.body)
+        expect(page).not_to have_content(blog2.title)
+        expect(page).not_to have_content(blog2.body)
+        
+        expect(page).not_to have_link(blog1.title)
+        expect(page).not_to have_link(blog2.title)
+
+        within ("h1#no-posts") do
+            expect(page).to have_content("No Blog Posts Created")
+        end
+    end
 end
